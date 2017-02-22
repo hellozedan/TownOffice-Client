72e9f0e0bceec65171efb7ae4c136916f6bdd4fc
Ahmad Zedany <zedany7@hotmail.com>
2017-02-22 22:55:53 +0200

    Initial commit

---
+++ app/config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by kholod on 22/02/2017.
+ */
+var appConfig = {
+    title: "Office",
+    apiBase: 'https://librarykholod.herokuapp.com/api/',
+    /*    apiBase: 'http://localhost:3000/',*/
+
+};
---
+++ app/view_form/view_form.js
@@ -0,0 +1,92 @@
+'use strict';
+
+angular.module('myApp.form1', ['ngRoute'])
+
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/form1', {
+            templateUrl: 'form1/form1.html',
+            controller: 'Form1Ctrl'
+        });
+    }])
+    .controller('Form1Ctrl', Form1Ctrl);
+
+function Form1Ctrl($scope,FormDetailsService) {
+
+
+    $scope.form1 = {};
+    $scope.form1.date = new Date();
+    $scope.form1.name="";
+
+
+    $scope.printPage = function () {
+        window.print();
+    }
+
+    $scope.form1.table = [
+
+        {
+            col1: '  ',
+            col2: '  ',
+            col3: '',
+            col4: '  ',
+            col5: '',
+            col6: '',
+            col7: ''
+        },
+        {
+            col1: '',
+            col2: '',
+            col3: '',
+            col4: '',
+            col5: '',
+            col6: '',
+            col7: ''
+        },
+        {
+            col1: '',
+            col2: '',
+            col3: '',
+            col4: '',
+            col5: '',
+            col6: '',
+            col7: ''
+        },
+        {
+            col1: '',
+            col2: '',
+            col3: '',
+            col4: '',
+            col5: '',
+            col6: '',
+            col7: ''
+        }
+    ]
+
+
+    $scope.addRowToTable = function () {
+        $scope.form1.table.push({
+            col1: '',
+            col2: '',
+            col3: '',
+            col4: '',
+            col5: '',
+            col6: '',
+            col7: ''
+        })
+    }
+
+    $scope.removeRowFromTable = function (index) {
+        $scope.form1.table.pop();
+    }
+
+
+    $scope.saveForm=function () {
+        $scope.form1.type="water";
+        FormDetailsService.create($scope.form1).then(function (result) {
+                console.log('saved');
+            }, function (err) {
+            }
+        );
+    }
+}
+
